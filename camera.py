3a3e6fd1709538b5c26d9267a4eb6c6e0d8ef526
Joao Fernandes Lopes <61229146+JUSTIKA@users.noreply.github.com>
2025-04-09 13:18:47 +0200

    Component detection V2 ( train 13 )
    Added feature extraction algorithm to the train 11 model, more efficient and accurate detection

--- camera.py
+++ camera.py
@@ -1,46 +1,43 @@
 from ultralytics import YOLO
 import cv2
 
-# Load your trained YOLOv8 model
-model = YOLO("/home/joao/Documents/GitHub/yolo/runs/detect/train11/weights/best.pt")
+# model = YOLO("/home/joao/Documents/GitHub/yolo/Elec_Compo_ide/runs/detect/train11/weights/best.pt") #model 11
+model = YOLO("/home/joao/Documents/GitHub/yolo/Elec_Compo_ide/runs/detect/train13/weights/best.pt")
 
-# Open the camera
-cap = cv2.VideoCapture(2)  # 0 represents the default camera
+cap = cv2.VideoCapture(0)  # 0 represents the default camera
 
-# Set the desired frame width and height (optional, depends on your camera)
+if not cap.isOpened():
+    print("Error: Could not open the camera.")
+    exit()
+
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-# Define the codec and create a VideoWriter object to save the output
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for .mp4 files
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))  # 20 FPS, resolution 640x480
+# Define the codec and create a VideoWriter object to save the video
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for .avi files
+# out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))  # 20 FPS, resolution 640x480
 
 while cap.isOpened():
     success, frame = cap.read()
 
-    if success:
-        # Resize the frame to a smaller size for better performance
-        resized_frame = cv2.resize(frame, (640, 480))  # Resize to 640x480
-
-        # Perform object detection on the resized frame
-        results = model(resized_frame)
-
-        # Annotate the frame with detection results
-        annotated_frame = results[0].plot()
-
-        # Write the annotated frame to the output video file
-        out.write(annotated_frame)
-
-        # Display the annotated frame
-        cv2.imshow("YOLOv8 Object Detection", annotated_frame)
-
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    else:
+    if not success:
+        print("Error: Could not read frame from the camera.")
         break
 
-# Release the camera and video writer, and close the window
+    resized_frame = cv2.resize(frame, (640, 480))  # Resize to 640x480
+
+    results = model(resized_frame)
+
+    annotated_frame = results[0].plot()
+
+    # Write the annotated frame to the output video file
+    # out.write(annotated_frame)
+
+    cv2.imshow("YOLOv8 Object Detection", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
 cap.release()
-out.release()
+# out.release()
 cv2.destroyAllWindows()
