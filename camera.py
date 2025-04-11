3172c76af1f9722a5b79279c6991ba6af95513c7
Joao Fernandes Lopes <61229146+JUSTIKA@users.noreply.github.com>
2025-04-11 14:51:53 +0200

    Last good model up to date train21 trained on V3

--- camera.py
+++ camera.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 # model = YOLO("/home/joao/Documents/GitHub/yolo/Elec_Compo_ide/runs/detect/train11/weights/best.pt") #model 11
-model = YOLO("/home/joao/Documents/GitHub/yolo/Elec_Compo_ide/runs/detect/train18/weights/best.pt")
+model = YOLO("/home/joao/Documents/GitHub/yolo/Elec_Compo_ide/Test sets/train8/weights/best.pt")
 
 cap = cv2.VideoCapture(0)  # 0 represents the default camera
 
@@ -14,8 +14,8 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # Define the codec and create a VideoWriter object to save the video
-# fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for .avi files
-# out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))  # 20 FPS, resolution 640x480
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for .avi files
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))  # 20 FPS, resolution 640x480
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -31,7 +31,7 @@ while cap.isOpened():
     annotated_frame = results[0].plot()
 
     # Write the annotated frame to the output video file
-    # out.write(annotated_frame)
+    out.write(annotated_frame)
 
     cv2.imshow("YOLOv8 Object Detection", annotated_frame)
 
